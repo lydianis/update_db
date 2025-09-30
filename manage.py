d7001966755acab4d4b02aebe4fc7f0b97a303fb
lydianis <Lydia.Nisius@gmail.com>
2025-09-30 10:47:09 +0200

    update_db_initial_setup

--- manage.py
+++ manage.py
@@ -9,9 +9,6 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'update_db.settings')
     try:
         from django.core.management import execute_from_command_line
-        # Override default port for `runserver` command
-        from django.core.management.commands.runserver import Command as runserver
-        runserver.default_port = "8080"
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -23,6 +20,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
--- update_db/settings.py
+++ update_db/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o=7m&g6r2sp^u*4#!_r73x0a91dq9an!c#0gbe)enqk_msjtj1'
+SECRET_KEY = 'django-insecure-mrfzz&^&rv@m_(h*z@_pni#k$9jbiwb=q)aiy8#1wo^w3_wu%g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
